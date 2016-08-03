402d18c690ccad5cabf2676dd73b89364a3883ee
Mihai Nicolae <mihai.nicolae@intel.com>
2016-08-03 15:36:10 +0300

    Added MyMessages.java for solving week 2 assignment

---
+++ JavaTraining/src/messages/MyMessages.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package messages;
+
+/**
+ *
+ * @author mnicolae
+ */
+public class MyMessages {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This my first Java Program");
+        System.out.println("So, hello Java it is.");
+        System.out.println("Java is WORA!");      
+    }
+    
+}

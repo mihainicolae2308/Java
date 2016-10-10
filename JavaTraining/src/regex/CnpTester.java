0b71f7e588a12049c35a8f2ae8d46b4a9dc65264
Mihai <Mihai@DESKTOP-L9904VH>
2016-10-10 22:58:43 +0300

    Added code for completing regex task.

---
+++ JavaTraining/src/regex/CnpTester.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Mihai
+ */
+public class CnpTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //Method for verifying the correct month legth (the regular expression allows the interval [1-31] for all months)
+    public static boolean extraDateValidation(String cnp) {
+        int month = Integer.parseInt(cnp.substring(3,5));
+        int day = Integer.parseInt(cnp.substring(5,7));
+        if ((((month == 2) || (month == 6) || (month == 9) || (month == 11)) && (day == 31)) || ((month == 2) &&((day == 30) || (day == 31)))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    //Method for testing the CNP format based on a regular expresion
+    public static boolean testCnp(String regex, String cnp) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(cnp);
+        boolean isMonthLengthCorrect = extraDateValidation(cnp);
+        // verify if the inserted CNP matches the regular expresion and the correct month length verified by extraDateValidation() method
+        if (matcher.matches() && isMonthLengthCorrect) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        String regex = "[1-2][0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[1-9]|2[1-9]|3[0-1])[0-9]{6}";
+        
+        // read the source string using a new Scanner object
+        Scanner scannerSourceString = new Scanner(System.in);
+        System.out.println("Insert CNP: ");
+        String source = scannerSourceString.next();
+        
+        
+        boolean isCnpValid = testCnp(regex, source);
+        if (isCnpValid) {
+            System.out.println("CNP valid");
+        } else {
+            System.out.println("CNP invalid");
+        }
+    }
+    
+}
---
+++ JavaTraining/test/regex/CnpTesterIT.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regex;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mihai
+ */
+public class CnpTesterIT {
+    
+    public CnpTesterIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class CnpTester.
+     */
+//    @Test
+//    public void testMain() {
+//        System.out.println("main");
+//        String[] args = null;
+//        CnpTester.main(null);
+//        // TODO review the generated test code and remove the default call to fail.
+//        //fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Tests of extraDateValidation method, of class CnpTester.
+     */
+    
+    //Testing a valid date
+    @Test
+    public void testExtraDateValidation() {
+        System.out.println("extraDateValidation");
+        String cnp = "1800831213724";
+        boolean expResult = true;
+        boolean result = CnpTester.extraDateValidation(cnp);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    //Test with 31-06 (invalid date)
+    @Test
+    public void testExtraDateValidation2() {
+        System.out.println("extraDateValidation");
+        String cnp = "1800631213724";
+        boolean expResult = false;
+        boolean result = CnpTester.extraDateValidation(cnp);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+    //Test with 30-02 (invalid date)
+    @Test
+    public void testExtraDateValidation3() {
+        System.out.println("extraDateValidation");
+        String cnp = "1800230213724";
+        boolean expResult = false;
+        boolean result = CnpTester.extraDateValidation(cnp);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+
+    /**
+     * Tests of testCnp method, of class CnpTester.
+     */
+    //Test with an invalid starting number
+    @Test
+    public void testTestCnp() {
+        System.out.println("testCnp");
+        String regex = "[1-2][0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[1-9]|2[1-9]|3[0-1])[0-9]{6}";
+        String cnp = "3800823213724";
+        boolean expResult = false;
+        boolean result = CnpTester.testCnp(regex, cnp);
+        assertEquals(expResult, result);
+    }
+    
+    //Test that a valid CNP works
+    @Test
+    public void testTestCnp2() {
+        System.out.println("testCnp");
+        String regex = "[1-2][0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[1-9]|2[1-9]|3[0-1])[0-9]{6}";
+        String cnp = "1800823213724";
+        boolean expResult = true;
+        boolean result = CnpTester.testCnp(regex, cnp);
+        assertEquals(expResult, result);
+    }
+
+    //Test with an invalid day (>31)
+    @Test
+    public void testTestCnp3() {
+        System.out.println("testCnp");
+        String regex = "[1-2][0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[1-9]|2[1-9]|3[0-1])[0-9]{6}";
+        String cnp = "1800832213724";
+        boolean expResult = false;
+        boolean result = CnpTester.testCnp(regex, cnp);
+        assertEquals(expResult, result);
+    }
+    
+    //Test with an invalid month (>12)
+    @Test
+    public void testTestCnp4() {
+        System.out.println("testCnp");
+        String regex = "[1-2][0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[1-9]|2[1-9]|3[0-1])[0-9]{6}";
+        String cnp = "1801323213724";
+        boolean expResult = false;
+        boolean result = CnpTester.testCnp(regex, cnp);
+        assertEquals(expResult, result);
+    }
+    
+    //Test with a longer number (length > 13)
+    @Test
+    public void testTestCnp5() {
+        System.out.println("testCnp");
+        String regex = "[1-2][0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[1-9]|2[1-9]|3[0-1])[0-9]{6}";
+        String cnp = "18008232137241";
+        boolean expResult = false;
+        boolean result = CnpTester.testCnp(regex, cnp);
+        assertEquals(expResult, result);
+    }
+    
+}
